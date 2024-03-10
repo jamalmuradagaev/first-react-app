9d63caf1f283a9822dcc6bf0eee85d52aedaa66b
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-03-10 20:53:04 +0300

    работа с запросами (backend) и использование useCallback

--- src/EffectSection/EffectSection.jsx
+++ src/EffectSection/EffectSection.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import Button from '../Button/Button'
 import Modal from '../Modal/Modal'
 
 export default function EffectSection() {
   const [modal, setModal] = useState()
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  function openModal(){
-    setModal(true)
-  }
+  // этот код можно записать внутри useEffect   
+  const fetchUsers = useCallback(async() => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users') 
+    const data = await response.json()
+    setUsers(data)
+    setLoading(false)
+  }, [])
+
+//   async function fetchUsers(){
+//     setLoading(true)
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users') 
+//     const data = await response.json()
+//     setUsers(data)
+//     setLoading(false)
+//   }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
+  
   return (
     <section>
       <h3>Эффекты</h3>
 
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button onClick={() => setModal(true)} style={{marginBottom: '1rem'}}>Открыть информацию</Button>
 
       <Modal open={modal}>
         <h3>Привет из модалки</h3>
@@ -20,6 +40,12 @@ export default function EffectSection() {
 
         <Button onClick={() => {setModal(false)}}>Закрыть модалку</Button>
       </Modal>
+
+      { loading && <p>Loading...</p> }
+
+      { !loading && <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>}
     </section>
   )
 }
--- src/TabsSection/TabsSection.jsx
+++ src/TabsSection/TabsSection.jsx
@@ -7,7 +7,7 @@ export default function TabsSection({active, onChange}) {
       <Button isActive={active === 'main'} onClick={() => onChange('main')}>Главная</Button>
       <Button isActive={active === 'feedback'} onClick={() => onChange('feedback')}>Обратная связь</Button>
 
-      <Button isActive={active === 'effect'} onClick={() => onChange('effect')}>Обратная связь</Button>
+      <Button isActive={active === 'effect'} onClick={() => onChange('effect')}>Эффекты</Button>
     </section>
   )
 }
